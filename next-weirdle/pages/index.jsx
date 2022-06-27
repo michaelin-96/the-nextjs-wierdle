6ad6803f3c37e59b5267a7f98b35b6292b8a7f11
Michael Lin <michaelin.96@gmail.com>
2022-06-27 09:02:35 -0700

    Nextjs Setup

---
+++ next-weirdle/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+    <Head>
+      <title>Weirdle</title>
+    </Head>
+      <h1>Welcome to Next</h1>
+    </div>
+  )
+}
